c570467baafb2c0843031728e9b67dd37499af0e
SAEEDDAYOUNGBEAST <saeedthebeast7@gmail.com>
2025-11-16 09:47:32 -0800

    Imports`
    import list and creation for class Storage

--- task_manager_cli/storage.py
+++ task_manager_cli/storage.py
@@ -1,2 +1,9 @@
 # This module handles loading tasks, saving tasks, and Converting them bwtween diffrent objects
 
+import json
+from pathlib import Path
+from  typing import List
+from .models import Task
+
+class Storage
+
